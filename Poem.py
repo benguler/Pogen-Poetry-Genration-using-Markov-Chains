d2fbc6267ba2906bbec2f43a63507005bd0bed11
benguler <50894078+benguler@users.noreply.github.com>
2020-11-22 18:21:18 -0500

    Add files via upload

--- Poem.py
+++ Poem.py
@@ -1,24 +1,39 @@
  import MarkovAgent
  
  class Poem:
-    def _init_(matrix, words, initWord, numLines, numWords):
+    def _init_(matrix, words, initWord, numLines, numWords, catagory):
         self.matrix = matrix
         self.words = words
         self.initWord = initWord
         self.numLines = numLines
         self.numWords = numWords
+        self.catagory = catagory
         
-    def generatePoem():
+    def generatePoem(iterations):
         agent = MarkovAgent(self.matrix, self.words, self.initWord)
         poem = ""
         
         for i in range(self.numLines):
-            for j in range(self.numWords):
-                poem += agent.getWord() + " "
+            
+            bestLine = ""
+            
+            for j in range(iterations):
+                tmpLine = ""
                 
-                agent.transition()
-                
+                for k in range(self.numWords):
+                    tmpLine += agent.getWord() + " "
+                    
+                    agent.transition()
+                    
+                if(nbDist(line) > nbDist(bestLine)):
+                    bestLine = line
+                    
+            poem += bestLine
             poem += "\n"
             
         return poem
+        
+    def nbDist(line):
+        #Get NaiveBayes probability distance for catagory 
+        return 0
         
