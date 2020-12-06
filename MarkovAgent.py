831b74772ca4983c3525459b3c19d6c791400bd7
benguler <50894078+benguler@users.noreply.github.com>
2020-12-06 15:48:34 -0500

    Add files via upload

--- MarkovAgent.py
+++ MarkovAgent.py
@@ -2,21 +2,28 @@ import random
 from markov import MarkovMatrix
 
 class MarkovAgent:
-    def __init__(self, markov, initState):
-        self.markov = markov
+    def __init__(self, markovMatrix, initState):
+        #Params: Markov probability matrix, initial state for agent
+        #State is a tuple in the form of (word1, ..., wordn), where n is the state size
+        self.markovMatrix = markovMatrix
         self.state = initState
-        self.markov = markov
-        self.stateSize = markov.state_size
+        self.stateSize = markovMatrix.state_size
     
     def transition(self):
-        newState = self.state[1:self.stateSize] + (self.markov.move(self.state), )
+        #Transition to a new state using the Markov matrix
+        #New state is the old state minus the first word plus the new word
+        #e.g. (w1, w2, w3) -> (w2, w3, w4)
+        newState = self.state[1:self.stateSize] + (self.markovMatrix.move(self.state), )
     
         self.state = newState
         
     def getState(self):
+        #Return current state
         return self.state
         
     def setState(self, state):
+        #Param: new state
+        #Set agent's current state
         self.state = state
         
     
--- Poem.py
+++ Poem.py
@@ -4,64 +4,63 @@ from PoemUtility import PoemUtility
 import syllables
 
 class Poem:
-    def __init__(self, markov, initState, numLines, numSyl, category):
-        #params: markov matrix, initial state, number of lines, number of syllables per line, category of poem
-        self.markov = markov
-        self.initState = initState
+    def __init__(self, markovMatrix, numLines, numSyl, category):
+        #params: markov probability matrix, number of lines, number of syllables per line, category of poem
+        self.markovMatrix = markovMatrix
         self.numLines = numLines
         self.numSyl = numSyl
         self.category = category
         
     def generatePoem(self, iterations):
+        
         #param: Number of iteration to run Naive-Bayes classification
         #Generate poem given specifications in constructor
-        agent = MarkovAgent(self.markov, self.initState)
+        agent = MarkovAgent(self.markovMatrix, self.genSeed())
         poem = ""
         
         #For each line of the poem
-        for i in range(self.numLines):   
+        for i in range(self.numLines):  
             bestLine = ""
             
             #Generate line for poem and run nb classification for num of iterations
             for j in range(iterations):
                 tmpLine = ""
-                seed = agent.getState()
                 
                 sylCount = 0
                 
+                #Add initial state to poem, minus the last word of that state
                 for i in range(len(agent.getState()) - 1):
                     tmpLine += agent.getState()[i] + " "
                     sylCount += syllables.estimate(agent.getState()[i])
                 
-                sylCount = syllables.estimate(tmpLine)
-                
                 #Generate line with roughly correct number of syllables
-                while(sylCount < self.numSyl[i]):
-                    tmpLine += agent.getState()[len(agent.getState()) - 1] + " "
-                    sylCount += syllables.estimate(agent.getState()[len(agent.getState()) - 1])
-                    agent.transition()
-                    
-                    """
-                    if(sylCount > self.numSyl[i]):
-                        print(tmpLine)
+                #len(agent.getState()) - 1] == "___END__") implies line is finished
+                while(not(sylCount == self.numSyl[i] and agent.getState()[len(agent.getState()) - 1] == "___END__")):
+                    if(sylCount > self.numSyl[i] or agent.getState()[len(agent.getState()) - 1] == "___END__"):    #If number of syllables has been surpassed or finished line has too few syllables 
+                        #Start new line with new seed
+                        agent.setState(self.genSeed())
+                        
                         tmpLine = ""
-                        agent.setState(seed)
                         
                         sylCount = 0
                         
                         for i in range(len(agent.getState()) - 1):
                             tmpLine += agent.getState()[i] + " "
-                            
-                        sylCount = syllables.estimate(tmpLine)
-                    """
+                            sylCount += syllables.estimate(agent.getState()[i])
                     
-                if(self.nbDist(tmpLine) >= self.nbDist(bestLine)):
+                    tmpLine += agent.getState()[len(agent.getState()) - 1] + " "    #Add the last word of the agent's current state to the line
+                    sylCount += syllables.estimate(agent.getState()[len(agent.getState()) - 1]) #Record its syllable count
+                    
+                    agent.transition()  #Have the agent transition to a new state
+                   
+                if(self.nbDist(tmpLine) >= self.nbDist(bestLine)):  #If the current line scores better or equal to the best line
+                    #best line = current line
                     bestLine = tmpLine
+                    
+                agent.setState(self.genSeed())  #Initialize seed for next iteration/line
                     
             poem += bestLine
             poem += "\n"
-            
-            agent.transition()
             
         return poem
         
@@ -70,5 +69,9 @@ class Poem:
         #Get NaiveBayes probability distribution for category 
         #return PoemUtility.classifySentence(line, self.category)
         
+        #Return 0 when not testing nb classification. Training takes too long
         return 0
+        
+    def genSeed(self):
+        return tuple(self.markovMatrix.walk(None, self.markovMatrix.state_size))
     
