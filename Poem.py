28dc52c74c54739b2f04cda574b6c95bc45e44cf
benguler <50894078+benguler@users.noreply.github.com>
2020-12-14 19:03:01 -0500

    Add files via upload

--- Poem.py
+++ Poem.py
@@ -9,25 +9,25 @@ BEGIN = "___BEGIN__"
 END = "___END__"
 
 class Poem:
-    def __init__(self, markovMatrix, numSyls, category):
+    def __init__(self, markovMatrix, numSyls, category, sBool):
         #params: markov probability matrix, number of syllables per line, category of poem (period_genre), bool that dictates if poem will have rhyming
         self.markovMatrix = markovMatrix
         self.numSyls = numSyls
         self.category = category
+        self.sBool = sBool
 
     def generatePoem(self):
         #Generate poem given specifications in constructor
         agent = MarkovAgent(self.markovMatrix, self.genSeed())  #Create agent with poem seed as initial state
         poem = ""
+        lines = []
         
         #For each line of the poem
         for syln in self.numSyls:  
-            line = ""
-        
             score = 0
             
             #Generate line for poem and run nb classification until nb score is enough
-            while(score < 0.2):
+            while(score < 0.7):
                 line = ""
                 
                 sylCount = 0
@@ -40,28 +40,40 @@ class Poem:
                 #Generate line with roughly correct number of syllables
                 #agent.stateSize - 1] == END) implies line is finished
                 
-                while(agent.getState()[agent.stateSize - 1] != END):                 
+                while(not((sylCount == syln if self.sBool else True) and agent.getLastWord() == END)):
+                    if((sylCount > syln if self.sBool else False) or (agent.getLastWord() == END if self.sBool else False)):    #If number of syllables has been surpassed or finished line has too few syllables 
+                        #Restart with new line with new seed
+                        agent.setState(self.genSeed())
+                        
+                        line = ""
+                        
+                        sylCount = 0
+                        
+                        for i in range(agent.stateSize - 1):
+                            line += agent.getState()[i] + " "
+                            sylCount += syllables.estimate(agent.getState()[i])
+                    
                     line += agent.getLastWord() + " "    #Add the last word of the agent's current state to the line
                     sylCount += syllables.estimate(agent.getLastWord()) #Update overall syllable count
                     
-                    agent.transition()  #Have the agent transition to a new state
-                    
-                score = self.nbDist(line)
+                    agent.transition()  #Have the agent transition to a new state for next round of testing/next line
                 
-                agent.setState(self.genSeed()) #Initialize agent state to new seed for next line/round of nb classification
-            
-            print("TEST 8")
+                if(line not in lines):
+                    score = self.nbDist(line)
+                    agent.setState(self.genSeed())  #Initialize agent state to new seed for next line/round of nb classification
+                    lines += [line]
+                    
+                else:
+                    score = 0
+
             poem += line
             poem += "\n"
             
-        print("TEST 3")
         return poem
     
     def genSeed(self):
         #Generate initial n words of the poem
         seed = tuple(self.markovMatrix.walk(None, self.markovMatrix.state_size))
-        
-        print("TEST 4")
         
         if(len(seed) != self.markovMatrix.state_size): #If seed is wrong state size
             return self.genSeed()   #Retry
@@ -69,10 +81,8 @@ class Poem:
         return seed
         
     def nbDist(self, line):
-        print("TEST 9")
         #param: line to run Naive-Bayes classification on
         #Get NaiveBayes probability distribution for category
-        print(line, " ", PoemUtility.classifySentence(line, self.category))
         return PoemUtility.classifySentence(line, self.category)
         
         #Return 0 when not testing nb classification. Training takes too long
